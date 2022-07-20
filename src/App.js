6a5b6fcbac71d41312320371886fbb01e6f5dc2f
Kristine Zhang <kristine.zhang@cognosante.com>
2022-07-20 13:10:56 -0700

    props in components

--- src/App.js
+++ src/App.js
@@ -1,35 +1,14 @@
+import Header from './components/Header'
+
 function App() {
-    const title = 'Blog Post'
-    const body = 'This is my blog post'
-    const comments = [
-        {id: 1, text: 'Comment one'},
-        {id: 2, text: 'Comment two'},
-        {id: 3, text: 'Comment three'},
-    ]
-
-    const loading = false
-    const showCommments = true
-
-    if(loading) return <h1>Loading...</h1>
-
-    const commentBlock = (        
-        <div className="comments">
-            <h3>Comments ({comments.length})</h3>
-            <ul>
-                {comments.map((comment, index) => (
-                    <li key={index}>{comment.text}</li>
-                ))}
-            </ul>
-        </div>)
-
     return (
-    <div className='container'>
-        <h1>{title.toUpperCase()}</h1>
-        <p>{body}</p>
-
-        {showCommments && commentBlock}
-    
-    </div>
+    <>
+        {/* pass props into component Header */}
+        <Header text="Hello World"/>
+        <div className='container'>
+            <h1>My App</h1>
+        </div>
+    </>
     )
 }
 
