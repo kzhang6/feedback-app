908980747cf79c37e3b5cbcef02feba40096fd8b
Kristine Zhang <kristine.zhang@cognosante.com>
2022-07-20 13:46:10 -0700

    defaultProp, propType
    Warning: Failed prop type: Invalid prop `text` of type `number` supplied to `Header`, expected `string`.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import Header from './components/Header'
+import PropTypes from 'prop-types';
 
 function App() {
     return (
     <>
         {/* pass props into component Header */}
-        <Header text="Hello World"/>
+        <Header text={3}/>
         <div className='container'>
             <h1>My App</h1>
         </div>
@@ -13,7 +14,11 @@ function App() {
 }
 
 Header.defaultProps = {
-    text: 'Feedback UI'
+    text: 'Feedback UI',
+}
+
+Header.propTypes = {
+    text: PropTypes.string
 }
 
 export default App
