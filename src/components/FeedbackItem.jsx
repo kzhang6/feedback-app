9bf7b03d9a5994c5b131a098b505e89180c456a3
Kristine Zhang <kristine.zhang@cognosante.com>
2022-07-22 11:56:11 -0700

    Pass in the deleteFeedback function as a prop from App -> FeedbackList -> FeedbackItem

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -2,12 +2,17 @@ import {FaTimes} from 'react-icons/fa'
 import PropTypes from 'prop-types';
 import Card from './shared/Card'
 
-function FeedbackItem({item}) {
+
+function FeedbackItem({item, handleDelete}) {
+
+  // const handleClick = (id) => {
+  //   console.log(id)
+  // }
 
     return (
     <Card>
         <div className='num-display'>{item.rating}</div>
-        <button className="close">
+        <button onClick={() => handleDelete(item.id)} className="close">
           <FaTimes color='purple'/>
         </button>
         <div className='text-display'>{item.text}</div>
