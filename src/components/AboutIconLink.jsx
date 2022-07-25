ddf0afc57fa9ea3ffe53ff3f17ca0b1ff46c475a
Kristine Zhang <kristine.zhang@cognosante.com>
2022-07-25 14:38:22 -0700

    Other optional features using the Link component
    (These optional parameters can be appended to the path name)

--- src/components/AboutIconLink.jsx
+++ src/components/AboutIconLink.jsx
@@ -5,7 +5,11 @@ import {Link} from 'react-router-dom'
 function AboutIconLink() {
   return (
     <div className='about-link'>
-        <Link to='/about'>
+        <Link to={{
+            pathname: '/about',
+            search: '?sort=name',
+            hash: '#hello'
+        }}>
             <FaQuestion size={30} />
         </Link>
     </div>
