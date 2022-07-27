0a4f8836dca69e2e96953351f70b776ae25882b7
Kristine Zhang <kristine.zhang@cognosante.com>
2022-07-27 15:05:33 -0700

    Add loading before displaying feedbacks

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -5,29 +5,29 @@ import FeedbackContext from '../Context/FeedbackContext'
 
 function FeedbackList() {
     /* getting data from FeedbackContext.Provider value rather than passed in as a prop from App.js */
-    const {feedback} = useContext(FeedbackContext) //
+    const {feedback, isLoading} = useContext(FeedbackContext) //
 
-    if (!feedback || feedback.length === 0) {
+    if (!isLoading && (!feedback || feedback.length === 0)) { //not loading, and no feedback or length is 0
         return <p>No Feedback Yet</p>
     }
 
-    return (
+    return isLoading ? <h3>Loading...</h3> : (
         <div className='feedback-list'>
-            <AnimatePresence>
-                {feedback.map((item) => (
-                    <motion.div 
-                        key={item.id}
-                        initial={{opacity: 0}}
-                        animate={{opacity: 1}}
-                        exit={{opacity: 0}}
-                    >                    
-                        <FeedbackItem key={item.id} item={item} />
-                    </motion.div>
-                ))}
-            </AnimatePresence>
+                    <AnimatePresence>
+                        {feedback.map((item) => (
+                            <motion.div 
+                                key={item.id}
+                                initial={{opacity: 0}}
+                                animate={{opacity: 1}}
+                                exit={{opacity: 0}}
+                            >                    
+                                <FeedbackItem key={item.id} item={item} />
+                            </motion.div>
+                        ))}
+                    </AnimatePresence>
 
-        </div>
-      )
+                </div>
+    )
 
 //without animation
 //     return (
