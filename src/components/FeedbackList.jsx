d9b207473be0edd91737d88b5366a2477927e2f1
Kristine Zhang <kristine.zhang@cognosante.com>
2022-07-29 14:58:30 -0700

    Revert previous revert, add back changes to simulate a backend server with POST/PUT/DELETE requests
    Revert "Revert all changes with the back-end json-server bb58052fc399f31223ac6ac36a3e39066affd84b."
    This reverts commit bfafbc31b117daa057cd65f470f3b7a9d48c195d.
    
    command used:
    git revert  --no-commit bfafbc31b117daa057cd65f470f3b7a9d48c195d
    
    then solved merge conflict on line 60 in FeedbackContext.js
    
    previous commit 209ad6db1a1b9d5f786c8a3ad62bc0fa923582e7 (without backend) is now the head of the deploy branch

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,33 +1,33 @@
 import {motion, AnimatePresence} from 'framer-motion'
 import { useContext } from 'react'
 import FeedbackItem from "./FeedbackItem"
+import Spinner from "./shared/Spinner"
 import FeedbackContext from '../Context/FeedbackContext'
 
 function FeedbackList() {
     /* getting data from FeedbackContext.Provider value rather than passed in as a prop from App.js */
-    const {feedback} = useContext(FeedbackContext) //
+    const {feedback, isLoading} = useContext(FeedbackContext) //
 
-    if (!feedback || feedback.length === 0) {
+    if (!isLoading && (!feedback || feedback.length === 0)) { //finished loading, and no feedback or length is 0
         return <p>No Feedback Yet</p>
     }
 
-    return (
+    return isLoading ? ( <Spinner /> )  : ( //load spinner component if isLoading
         <div className='feedback-list'>
-            <AnimatePresence>
-                {feedback.map((item) => (
-                    <motion.div 
-                        key={item.id}
-                        initial={{opacity: 0}}
-                        animate={{opacity: 1}}
-                        exit={{opacity: 0}}
-                    >                    
-                        <FeedbackItem key={item.id} item={item} />
-                    </motion.div>
-                ))}
-            </AnimatePresence>
-
-        </div>
-      )
+                    <AnimatePresence>
+                        {feedback.map((item) => (
+                            <motion.div 
+                                key={item.id}
+                                initial={{opacity: 0}}
+                                animate={{opacity: 1}}
+                                exit={{opacity: 0}}
+                            >                    
+                                <FeedbackItem key={item.id} item={item} />
+                            </motion.div>
+                        ))}
+                    </AnimatePresence>
+                </div>
+    )
 
 //without animation
 //     return (
